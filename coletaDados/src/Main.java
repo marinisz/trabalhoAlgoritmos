b3fb1cef2ba5e3bcf3b024a1e0e30bf8cb36d1e7
Vinícius Marini <marini_costa@hotmail.com>
2021-11-05 19:30:32 -0300

    Mudança na main

--- coletaDados/src/Main.java
+++ coletaDados/src/Main.java
@@ -1,49 +1,46 @@
 import carteira.Portifolio;
-import exceptions.opcaoInvalidaException;
-import investimentos.Indicadores;
 
-import javax.sound.sampled.Port;
 import java.io.IOException;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int opcao=-1;
-//        try {
-//            opcao = opcao();
-//            int tipo = -1;
-//            while(tipo<1 || tipo >3){
-//                Scanner teclado = new Scanner(System.in);
-//                System.out.println("Qual seu perfil de investidor?");
-//                System.out.print("1 - Conservador\n2 - Moderado\n3 - Agressivo\nMeu perfil: ");
-//                tipo = teclado.nextInt();
-//                if(tipo<1 || tipo >3){
-//                    System.out.println("Opção inválida");
-//                }
-//            }
-//            System.out.println("Gerando carteira com "+opcao+" ativos: ");
-            Portifolio portifolio1 = new Portifolio(3,1);
-            Portifolio portifolio2 = new Portifolio(3,2);
-            Portifolio portifolio3 = new Portifolio(3,3);
-            System.out.println(portifolio1);
-            System.out.println();
-            System.out.println(portifolio2);
-            System.out.println();
-            System.out.println(portifolio3);
+        String[] array = {"conservadora","moderada","agressiva"};
+        int quantidadeAtivos=-1;
+            while(quantidadeAtivos<1||quantidadeAtivos>10){
+                quantidadeAtivos = quantidadeAtivos();
+            }
+        int tipo = -1;
+            while(tipo<1 || tipo >3){
+                tipo = tipoPerfil();
+            }
+        System.out.println("\nGerando carteira de perfil "+array[tipo-1]+" com "+quantidadeAtivos+" ativos: ");
+        Portifolio portifolio1 = new Portifolio(quantidadeAtivos,tipo);
+        System.out.println(portifolio1);
             //para mudar o perfil tem que ir no indicadores na linha 158
-//        }catch (opcaoInvalidaException e) {
-//            System.out.println(e);
-//        }
+//
 
     }
-    public static int opcao() throws opcaoInvalidaException {
+    public static int quantidadeAtivos() {
         Scanner teclado = new Scanner(System.in);
         System.out.print("Escolha quantos ativos deseja na sua carteira (entre 1 e 10): ");
         int opcao =-1;
         opcao = teclado.nextInt();
-        if(opcao<1||opcao>10){
-                throw new opcaoInvalidaException("Valor fora do permitido");
+        if(opcao<1 || opcao >10){
+            System.out.println("Opção inválida");
         }
         return opcao;
+    }
+
+    public static int tipoPerfil(){
+        int tipo = -1;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Qual seu perfil de investidor?");
+        System.out.print("1 - Conservador\n2 - Moderado\n3 - Agressivo\nMeu perfil: ");
+        tipo = teclado.nextInt();
+        if(tipo<1 || tipo >3){
+            System.out.println("Opção inválida");
+        }
+        return tipo;
     }
 }
--- coletaDados/src/exceptions/opcaoInvalidaException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class opcaoInvalidaException extends Exception{
-    public opcaoInvalidaException(String msg){
-        super(msg);
-    }
-}
